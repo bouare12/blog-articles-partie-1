027cee10172ef03e6ecd7d3731933845f32ab5eb
Bandiougou Bouare <bandiougoubouare@gmail.com>
2022-05-02 12:26:21 +0200

    Affichage hello word avec annotations

--- src/Controller/helloController.php
+++ src/Controller/helloController.php
@@ -3,16 +3,23 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 
 
-class HelloController {
+class helloController {
 
+    /**
+     *@Route("/hello")
+     */
     public function hello (): Response {
         echo 'Hello Word';
         return new Response();
     }
 
+    /**
+     *@Route("hello/{name}")
+     */
     public function helloName ($name): Response {
         return new Response('Hello '. $name);
     }
