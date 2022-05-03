0cb11ea174864fc07bf20e453b763e993f269d1b
Bandiougou Bouare <bandiougoubouare@gmail.com>
2022-05-04 01:41:45 +0200

    Correction TP

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\ArticleRepository;
+use App\Repository\CategoryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,14 +12,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(ArticleRepository $articleRepository): Response
+    public function index(ArticleRepository $articleRepository, CategoryRepository $categoryRepository): Response
     {
         $articles = $articleRepository->findAll();
+        $categories = $categoryRepository->findAll();
 
         // dd($articles);
 
         return $this->render("home/index.html.twig", [
-            'articles' => $articles
+            'articles' => $articles,
+            'categories' => $categories
         ]);
     }
 
@@ -30,6 +34,30 @@ class HomeController extends AbstractController
 
         return $this->render("show/show.html.twig ", [
             'article' => $article
+        ]);
+    }
+
+    #[Route('/showArticle/{id}', name: 'show_Article')]
+    public function showArticle(?Category $categorie, CategoryRepository $categoryRepository): Response
+    {
+
+        $categories = $categoryRepository->findAll();
+
+        if($categorie) {
+
+            $articles = $categorie->getArticles()->getValues();
+
+        }else {
+
+            return $this->redirectToRoute('app_home');
+        }
+        // $categorie = $categoryRepository->find($id);
+
+        // dd($articles);
+
+        return $this->render("home/index.html.twig", [
+            'articles' => $articles,
+            'categories' => $categories
         ]);
     }
 
